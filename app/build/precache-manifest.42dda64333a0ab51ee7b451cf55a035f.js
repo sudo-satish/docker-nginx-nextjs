61c065f5a4ad88e19beaaef7c74e96fda29623b1
sg1995 <satish.gupta@clik.ai>
2021-05-13 10:21:49 +0530

    Working nginx config

---
+++ app/build/precache-manifest.42dda64333a0ab51ee7b451cf55a035f.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d482ce8bd32642851a0f5638848c9d",
+    "url": "/autouw-dev/index.html"
+  },
+  {
+    "revision": "d97301823da19c6d58c4",
+    "url": "/autouw-dev/static/css/2.052175f7.chunk.css"
+  },
+  {
+    "revision": "aa2df82c0e73f8e98cb6",
+    "url": "/autouw-dev/static/css/main.a794b581.chunk.css"
+  },
+  {
+    "revision": "d97301823da19c6d58c4",
+    "url": "/autouw-dev/static/js/2.e5431e87.chunk.js"
+  },
+  {
+    "revision": "aa2df82c0e73f8e98cb6",
+    "url": "/autouw-dev/static/js/main.2410897c.chunk.js"
+  },
+  {
+    "revision": "2507512793ace627c461",
+    "url": "/autouw-dev/static/js/runtime~main.f94c907d.js"
+  },
+  {
+    "revision": "1bc2ef35ff3167cead877a46f1c56f1a",
+    "url": "/autouw-dev/static/media/Gear-4s-60px.1bc2ef35.svg"
+  },
+  {
+    "revision": "8fb4b9c7524cbdbf8aecde3dc8777aec",
+    "url": "/autouw-dev/static/media/avatar.8fb4b9c7.svg"
+  },
+  {
+    "revision": "cf7f95781d44f365429f14f1c71ece89",
+    "url": "/autouw-dev/static/media/charge-code.cf7f9578.svg"
+  },
+  {
+    "revision": "9b4d4a28fb8f2e836443e3b644f0d8cb",
+    "url": "/autouw-dev/static/media/clikai-hero.9b4d4a28.svg"
+  },
+  {
+    "revision": "8be3e3cbddd9860e2c0cc839a5d5ed17",
+    "url": "/autouw-dev/static/media/clikai-icons-sprite-grey.8be3e3cb.svg"
+  },
+  {
+    "revision": "835fb4f5c88993f789ec5ceeecd3eb8c",
+    "url": "/autouw-dev/static/media/clikai-logo.835fb4f5.svg"
+  },
+  {
+    "revision": "3b84337357265042fff4ad4aff9c0534",
+    "url": "/autouw-dev/static/media/clikai-sprite-png-icons.3b843373.svg"
+  },
+  {
+    "revision": "bbfe38dbf3e71a812da30cfb5aa5a9ad",
+    "url": "/autouw-dev/static/media/close-button-icon.bbfe38db.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/autouw-dev/static/media/close-icon.cad70081.svg"
+  },
+  {
+    "revision": "b4149e8c38055f73f278136c766232a9",
+    "url": "/autouw-dev/static/media/col-resize.b4149e8c.svg"
+  },
+  {
+    "revision": "62a29c08f9db976a797510a2ddfeeb2d",
+    "url": "/autouw-dev/static/media/document-summary.62a29c08.svg"
+  },
+  {
+    "revision": "092babcc77c462dc53fc705ea8118837",
+    "url": "/autouw-dev/static/media/down-arrow-white.092babcc.svg"
+  },
+  {
+    "revision": "0f8f791dc05747096ea8066e0a10cba9",
+    "url": "/autouw-dev/static/media/excel-icon.0f8f791d.svg"
+  },
+  {
+    "revision": "5cb2cbc3dd793d83a87c4a0b33b1d4d7",
+    "url": "/autouw-dev/static/media/file-not-found.5cb2cbc3.svg"
+  },
+  {
+    "revision": "52bec027c7f74289b037e14c24ce44fc",
+    "url": "/autouw-dev/static/media/file-symbol.52bec027.svg"
+  },
+  {
+    "revision": "953165d394eca0b8693e4ea79473b27b",
+    "url": "/autouw-dev/static/media/floorPlan-unitType.953165d3.svg"
+  },
+  {
+    "revision": "7886aae1452a1c7b4232fa620ec155d6",
+    "url": "/autouw-dev/static/media/folder.7886aae1.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/autouw-dev/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/autouw-dev/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/autouw-dev/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/autouw-dev/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/autouw-dev/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "cfc6240fe63a98c2be7cb6be322951fc",
+    "url": "/autouw-dev/static/media/fx.cfc6240f.svg"
+  },
+  {
+    "revision": "84a6d488fca8f383b6cbbd1f4cfe4d9d",
+    "url": "/autouw-dev/static/media/key.84a6d488.svg"
+  },
+  {
+    "revision": "8d317898685887c3eb11ab64f27b6783",
+    "url": "/autouw-dev/static/media/linkedin.8d317898.svg"
+  },
+  {
+    "revision": "0ce314107f5b31a41351177483cdf179",
+    "url": "/autouw-dev/static/media/login-background.0ce31410.svg"
+  },
+  {
+    "revision": "ab4e3950d1c13f09517ddb800d9f2b4d",
+    "url": "/autouw-dev/static/media/merge-icon.ab4e3950.svg"
+  },
+  {
+    "revision": "00ef19ff1de54f11247bcf19d15d9547",
+    "url": "/autouw-dev/static/media/no-documents.00ef19ff.svg"
+  },
+  {
+    "revision": "eed4fbfa79f078f1f3742523d304ae8e",
+    "url": "/autouw-dev/static/media/note.eed4fbfa.svg"
+  },
+  {
+    "revision": "df68a0074afb2c8657465aae0d861a7f",
+    "url": "/autouw-dev/static/media/notification.df68a007.svg"
+  },
+  {
+    "revision": "d2de6c4bfbf928c2785cff606d5dfe77",
+    "url": "/autouw-dev/static/media/password.d2de6c4b.svg"
+  },
+  {
+    "revision": "58f7347f4a5731d5404b6d3956eeb1a1",
+    "url": "/autouw-dev/static/media/pdf.58f7347f.svg"
+  },
+  {
+    "revision": "6075b416bf14fa9b10b0358344e1778c",
+    "url": "/autouw-dev/static/media/privacy-policy.6075b416.svg"
+  },
+  {
+    "revision": "fe61049807d03baffb4982f72df9f766",
+    "url": "/autouw-dev/static/media/profile.fe610498.svg"
+  },
+  {
+    "revision": "66d062025e76bd77e8eb75f0a0daab61",
+    "url": "/autouw-dev/static/media/right-arrow.66d06202.svg"
+  },
+  {
+    "revision": "551fd51fd3f60433c215fbfa1ee1020e",
+    "url": "/autouw-dev/static/media/tenant-type.551fd51f.svg"
+  },
+  {
+    "revision": "209e27e8f7ce09ec110fbe22f7165d4c",
+    "url": "/autouw-dev/static/media/term.209e27e8.svg"
+  },
+  {
+    "revision": "651ec6c49b98d50c37ed57e57a6206b0",
+    "url": "/autouw-dev/static/media/text-documents.651ec6c4.svg"
+  },
+  {
+    "revision": "d36dd9debe890ab013e1ec2e9dd47b23",
+    "url": "/autouw-dev/static/media/tutorial.d36dd9de.svg"
+  },
+  {
+    "revision": "b3ec4ce6b751fcba678cde340d8b6551",
+    "url": "/autouw-dev/static/media/unit-status.b3ec4ce6.svg"
+  },
+  {
+    "revision": "c010b4413b422d86f2b78792f5af78cf",
+    "url": "/autouw-dev/static/media/upload-doc.c010b441.svg"
+  }
+]);
--- readme.md
+++ readme.md
@@ -8,5 +8,8 @@ docker build --tag autouw-nginx .
 ### Delete image
 docker image rm autouw-nginx
 
+### Delete container
+docker container rm autouw-nginx -f
+
 ### Run container
 docker run -d -p 80:80 --name autouw-nginx autouw-nginx
